102627e78b5cbbe4aa077bb29af5803731ae0d80
Vishwadeep <94741874+Vishwa-3047@users.noreply.github.com>
2024-08-11 19:53:53 +0530

    aug9.java

--- August 09-08-2024/aug9.java
+++ August 09-08-2024/aug9.java
@@ -3,21 +3,15 @@ class student
 {
 	student(int a, String b)
 	{
-		System.out.println("Number : "+a);
-		System.out.println("Name : "+b);
+		System.out.println("Number : "+a+" "+"Name : "+b);
 	}
 	student(int c, String d, int e)
 	{
-		System.out.println("Number : "+c);
-		System.out.println("Name : "+d);
-		System.out.println("Number : "+e);
+		System.out.println("Number : "+c+" "+"Name : "+d+" "+"Number : "+e);
 	}
 	student(int f, String g, int h, int i)
 	{
-		System.out.println("Number : "+f);
-		System.out.println("Name : "+g);
-		System.out.println("Number : "+h);
-		System.out.println("Number : "+i);
+		System.out.println("Number : "+f+" "+"Name : "+g+" "+"Number : "+h+" "+"Number : "+i);
 	}
 }
 public class aug9 
@@ -25,7 +19,7 @@ public class aug9
 	public static void main(String[] args) 
 	{
 		student ob=new student(10, "Vishwa");
-		/*student obj=new student(20, "Archana", 30);
-		student obje=new student(40, "Priya", 50, 60);*/
+		student obj=new student(20, "Archana", 30);
+		student obje=new student(40, "Aradhya", 50, 60);
 	}
 }
